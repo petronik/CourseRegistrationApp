3babd99766710a3b253754235bee4b010a1a4701
Petronik <petronik@ukr.net>
2021-08-26 09:05:27 -0500

    addad validation

--- CourseRegistrationApp/ModelsDto/StudentDto.cs
+++ CourseRegistrationApp/ModelsDto/StudentDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,12 +12,17 @@ namespace CourseRegistrationApp.ModelsDto
         [DisplayName("Student Id")]
         public int S_Id { get; set; }
         [DisplayName("First Name")]
+        [Required]
         public string S_FirstName { get; set; }
         [DisplayName("Last Name")]
+        [Required]
         public string S_LastName { get; set; }
         [DisplayName("Email")]
+        [Required]
         public string S_Email { get; set; }
         [DisplayName("Phone Number")]
+        [Required]
+        [ StringLength  ( 10, ErrorMessage = "Maximum 10 digits")]
         public string S_PhoneNumber { get; set; }
 
     }
